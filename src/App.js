00142e739da0beb41d32451d4494df1b3ed2de0f
Lucas Souza <lucassouzafranco1@gmail.com>
2022-04-25 21:05:41 -0300

    Implementação da API

--- src/App.js
+++ src/App.js
@@ -3,13 +3,34 @@ import Header from './Components/Header/index.js';
 import 'normalize.css';
 import Main from './Components/Main/index.js'
 import Cards from './Components/Cards/index.js';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 const App = () => {
+
+  const [episodes, setEpisodes] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    async function loadData() {
+      setLoading(true);
+      await axios.get('https://rickandmortyapi.com/api/episode')
+        .then((response) =>{
+          if(response) {
+            setEpisodes(response.data.results);
+            setLoading(false);
+          }
+        })
+          .catch((erro) => console.log(erro));
+    }
+    loadData();
+  }, [])
+
   return (
     <>
       <Header/>
-      <Main/>
-      <Cards/>
+      <Main/> 
+      {loading ? <span>Carregando</span> : <Cards eps={episodes}/>}
     </>
   );
 }
--- src/Components/Cards/index.js
+++ src/Components/Cards/index.js
@@ -1,38 +1,30 @@
 import React from 'react';
 import './index.css';
-import rickandmorty_card_cover from '../../assets/Img/rickandmorty_card_cover.png';
-import {getEpisode} from 'rickmortyapi';
-import { useState } from 'react';
-import axios from 'axios';
-import {episodios} from '../../episodios';
 
-const Cards = (props) => {
-
-  const [episodeOne, setEpisodeOne] = React.useState((null), [props.episode]);
-  const [nameEpisode, setName] = React.useState((null), [props.name]);
-  
-  const loadData = async () => {
-    const request = await axios.get('https://rickandmortyapi.com/api/episode');
-    //setEpisodeOne(await getEpisode(1));
-    //console.log(episodeOne);
-  }
+const Cards = ({eps}) => {
 
   return(
+
     <>
       <div className='flexContainer'>
-      {episodios.map((item) => (
-        <div className='cardContainer'>
-          <div className='cardImage'>
-            <img src={rickandmorty_card_cover} className='imageStyle'></img>
+        {eps.map((item, key) => (
+          <div className='cardContainer'>
+            <div className='cardImage'>
+              <img src={`https://rickandmortyapi.com/api/character/avatar/${key+1}.jpeg`} className='imageStyle'></img>
+            </div>
+            <div className='cardText'>  
+              <div>
+                <p className='episodeTitle'>{item.name} - {item.episode}</p>
+              </div>
+              <div>
+                <p className='releaseDate'>{item.air_date}</p>
+              </div>
+            </div>
           </div>
-          <div className='cardText'>
-            <p className='textStyle'>{item.nome}</p>
-            <p className='releaseDate'>{item.temporada}</p>
-          </div>
+        ))}
         </div>
-      ))}
-      </div>
     </>
+
   )
 }
 
